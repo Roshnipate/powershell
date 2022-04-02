df99bba479ea622454655a81c1984499baf4054f
Roshnipate <100284346+Roshnipate@users.noreply.github.com>
2022-04-02 10:27:05 +0530

    Create greatest.ps1

---
+++ greatest.ps1
@@ -0,0 +1,15 @@
+$a=41
+$b=27
+$c=56
+if($a -gt $b -and $a -gt $c)
+{
+echo $a "Variable a is Greatest"
+}
+elseif($b -gt $a -and $b -gt $c)
+{
+echo $b "Variable b is Greatest"
+}
+else
+{
+echo "Variable c is Greatest"
+}
